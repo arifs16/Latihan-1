00764d639a2fa2c9f86a97cde2efdbf0290b01b8
arifs16 <arifsetyadi16@gmail.com>
2023-03-07 11:57:04 +0700

    latihan selesai

--- udemy-exercise1.py
+++ udemy-exercise1.py
@@ -2,19 +2,19 @@ print("Ibu memberi perintah, Beli 1 botol susu")
 print("Anak menjawab, OK")
 print("Anak pergi ke toko")
 
+jumlah_botol_susu = 173
+jumlah_telur = 0
+print(f"Jumlah botol susu {jumlah_botol_susu} botol")
+print(f"Jum;ah telur {jumlah_telur} butir")
 
-beli_susu = True
-if beli_susu:
+if jumlah_botol_susu > 0:
     print("Anak mengecek harganya cukup")
-else :
-    print("Anak tidak jadi membeli susu")
-
-
-beli_telur = True
-if beli_telur:
-    print("Anak membeli satu botol susu dan 6 butir telur")
+    if jumlah_telur > 0:
+        print("Anak membeli 1 botol susu dan 6 butir telur")
+    else:
+        print("Anak membeli 1 botol susu")
 else:
-    print("Anak membeli 1 botol susu")
+    print("Anak tidak jadi membeli susu")
 
 print("Anak pulang ke rumah")
 print("Menyerahkan belanjanya kepada Ibu")
